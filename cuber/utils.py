318b06a924be84fbfd1d02b446dc5c20e22bf882
Georgy Boresnko <gborisenko@yandex-team.ru>
2017-07-09 12:56:50 +0300

    hash fixes

--- cuber/utils.py
+++ cuber/utils.py
@@ -1,23 +1,39 @@
 import logging
 import json
 import numpy as np
+import hashlib
+import numbers
 logger = logging.getLogger(__name__)
+
+def sha224(s):
+    return hashlib.sha224(s).hexdigest()
+
+def object_hash(obj, fields = None):
+    if fields is not None:
+        d = {key: value for key, value in obj.__dict__.iteritems() if key in fields}
+    else:
+        d = obj.__dict__
+    return universal_hash(d)
 
 def universal_hash(obj):
     try:
-        return hash(obj)
+        return sha224(obj)
     except:
+        if isinstance(obj, numbers.Number):
+            return sha224(repr(obj))
         if isinstance(obj, dict):
-            return reduce(lambda x,y : x^y,
-                    [hash((universal_hash(key), universal_hash(value))) for key, value in obj.iteritems()]
+            return reduce(lambda x,y : universal_hash(x + y),
+                    [universal_hash((universal_hash(key), universal_hash(value))) for key, value in obj.iteritems()]
                 )
         if isinstance(obj, tuple):
-            return hash((universal_hash(item) for item in obj))
+            return json_hash(list(universal_hash(item) for item in obj))
         if isinstance(obj, list):
             return universal_hash(tuple(obj))
         if isinstance(obj, np.ndarray):
-            return hash(obj.tostring())
+            return sha224(obj.tostring())
+        if obj is None:
+            return sha224('none' + 'salt123')
         raise NotImplementedError()
 
 def json_hash(obj):
-    return hash(json.dumps(obj))
+    return sha224(json.dumps(obj))
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='cuber',
-      version='0.143',
+      version='0.15.3',
       description='Calculation-graph base',
       url='https://github.com/kriot/Cuber',
       author='Georgy Borisenko',
