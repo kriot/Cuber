ec89a382f7248c29dfd488f05b0163c7430459db
Georgy Boresnko <gborisenko@yandex-team.ru>
2017-08-04 11:09:45 +0300

    v0.23

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='cuber',
-      version='0.22.6',
+      version='0.23',
       description='Calculation-graph base',
       url='https://github.com/kriot/Cuber',
       author='Georgy Borisenko',
