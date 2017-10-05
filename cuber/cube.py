c11c093046a6760a14fda25a981ef89317f49858
Georgy Borisenko <gborisenko@yandex-team.ru>
2017-10-05 10:03:04 +0000

    Restorable cube flag

--- cuber/cube.py
+++ cuber/cube.py
@@ -12,6 +12,13 @@ class Cube(object):
 
     checkpoints_dir = 'checkpoints/'
 
+    '''
+    In derived cube may be set to false. 
+    If it is set to false, cube will be evalued every time. 
+    May be helpful, if cube generates window with plot, etc.
+    '''
+    restorable = True
+
     @abc.abstractmethod
     def name(self):
         '''
@@ -36,7 +43,7 @@ class Cube(object):
         data_file_cache = False
 
         # try load form memory
-        if not disable_inmemory_cache and not data_done:
+        if not disable_inmemory_cache and not data_done and self.restorable:
             cached, cached_data = cache.Cache().get(key)
             if cached: # true, if object is found 
                 logger.debug('Loaded from in-memory cache')
@@ -46,7 +53,7 @@ class Cube(object):
 
         # try load from file
         pickle_name = os.path.join(Cube.checkpoints_dir, '{}.pkl'.format(key))
-        if not disable_file_cache and not data_done:
+        if not disable_file_cache and not data_done and self.restorable:
             logger.info('Pickle name: {}'.format(pickle_name))
             if os.path.isfile(pickle_name):
                 logger.debug('Loading from file cache')
@@ -62,7 +69,7 @@ class Cube(object):
             data_done = True
 
         # save to file cache
-        if not disable_file_cache and not data_file_cache:
+        if not disable_file_cache and not data_file_cache and self.restorable:
             logger.debug('Save to file cache')
             if not os.path.isdir(Cube.checkpoints_dir):
                 os.makedirs(Cube.checkpoints_dir)
@@ -74,7 +81,7 @@ class Cube(object):
                 os.remove(pickle_name)
 
         # save to inmemory cache
-        if not disable_inmemory_cache and not data_inmemory_cache:
+        if not disable_inmemory_cache and not data_inmemory_cache and self.restorable:
             logger.debug('Save to inmemory cache')
             cache.Cache().add(key, data)
 
